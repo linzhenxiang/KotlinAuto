3331b5f9c4e782ce970a3f1f2c6f0e135af3756e
yr254gitLab <yr254@mail.yonrun.com>
2017-07-17 18:05:46 +0800

    save

---
+++ Auto/app/src/main/java/com/auto/bean/Note.kt
@@ -0,0 +1,25 @@
+package com.auto.bean
+
+/**
+ *
+ */
+class Note {
+    //贴子id
+    var n_id: String? = null
+    //user id
+    var u_id: String? = null
+    // car brand id
+    var brand_id: Int = 0
+    var models_id: Int = 0
+    var card_id: Int = 0
+    //note title
+    var title: String? = null
+    //上牌时间
+    var lis_time: Long = 0
+    //car color
+    var car_color: String? = null
+    var mileage: Float = 0f
+    var price: Float = 0f
+    //发布时间
+    var release_time: Long = 0
+}
